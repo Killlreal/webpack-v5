8a7bf02287fad5e7a1175601eccb05df39c3193e
Killlreal <sopochkin.kirill@yandex.ru>
2021-03-18 14:17:11 +0300

    fixed

--- src/vue/app.vue
+++ src/vue/app.vue
@@ -1,42 +1,42 @@
 <template lang="pug">
 #hello
-  .vue
+    .vue
     .vue__img
-      img(src="../img/1.png", alt="First") 
-      h1 img in vue
+        img(src="../img/1.png", alt="First") 
+        h1 img in vue
     .vue__button
-      .button(@click="clickAdd") {{ message }}
-      h1 button in vue
+        .button(@click="clickAdd") {{ message }}
+        h1 button in vue
     .vue__background
-      .background
-      h1 backgroun in vue
+        .background
+        h1 backgroun in vue
 </template>
- 
+
 <script>
 export default {
-  name: "app",
-  data() {
+    name: "app",
+    data() {
     return {
-      message: "button",
+        message: "button",
     };
-  },
-  methods: {
+    },
+    methods: {
     async clickAdd() {
-      let response = await fetch("https://reqres.in/api/products/3");
-      if (response.ok) {
+        let response = await fetch("https://reqres.in/api/products/3");
+        if (response.ok) {
         let json = await response.json();
         console.log(json);
-      } else {
+        } else {
         alert("Ошибка HTTP: " + response.status);
-      }
+        }
     },
-  },
+},
 };
 </script>
- 
+
 <style scoped>
 .button {
-  cursor: pounter;
-  border: 1px solid #000;
+    border: 1px solid #000;
+    cursor: pounter;
 }
 </style>
